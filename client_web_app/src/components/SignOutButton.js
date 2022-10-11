8fd9ff0791c5962304275116617ef5fcbc5b5c0e
Xuefeng Luo <hakunamatatahenry@gmail.com>
2022-10-11 14:18:43 +0200

    prettier

--- client_web_app/src/components/SignOutButton.js
+++ client_web_app/src/components/SignOutButton.js
@@ -3,18 +3,24 @@ import { useMsal } from "@azure/msal-react";
 import Button from "react-bootstrap/Button";
 
 function handleLogout(instance) {
-    instance.logoutRedirect().catch(e => {
-        console.error(e);
-    });
+  instance.logoutRedirect().catch((e) => {
+    console.error(e);
+  });
 }
 
 /**
  * Renders a button which, when selected, will redirect the page to the logout prompt
  */
 export const SignOutButton = () => {
-    const { instance } = useMsal();
+  const { instance } = useMsal();
 
-    return (
-        <Button variant="secondary" className="ml-auto" onClick={() => handleLogout(instance)}>Sign out using Redirect</Button>
-    );
-}
+  return (
+    <Button
+      variant="secondary"
+      className="ml-auto"
+      onClick={() => handleLogout(instance)}
+    >
+      Sign out using Redirect
+    </Button>
+  );
+};

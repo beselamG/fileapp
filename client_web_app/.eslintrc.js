964825e9369d441fe01b98bee690000f818c06bd
Xuefeng Luo <hakunamatatahenry@gmail.com>
2022-10-11 16:55:48 +0200

    change indent to 2

--- client_web_app/.eslintrc.js
+++ client_web_app/.eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   plugins: ["react"],
   rules: {
-    indent: ["error", 4],
+    indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
     quotes: ["error", "single"],
     semi: ["error", "always"],

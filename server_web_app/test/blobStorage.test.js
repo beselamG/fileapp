d28f77014cc5bb7a260721063df8ee8503e91006
beselam asefa <beselamasefa@NC-beselamasefa-CFCQMJ7TDQ.local>
2022-10-12 10:53:19 +0300

    blobstorage single test

---
+++ server_web_app/test/blobStorage.test.js
@@ -0,0 +1,23 @@
+const request = require("supertest");
+const app = require("../app.js");
+const api = request(app);
+
+const blobStorage = require("../blobStorage.js");
+
+beforeEach(() => {
+  const data = [{ contName: "name", blob: [] }];
+  mockDateNow = jest
+    .spyOn(blobStorage, "getContainerList")
+    .mockImplementation(() => data);
+});
+
+describe("GET /getContainer tes", () => {
+  test("should return the json", async () => {
+    await api
+      .get("/getContainer")
+      .expect(200)
+      .expect("Content-Type", /json/)
+      
+  });
+});
+
